7dbbfb644226b0c46760ae88390fd4130a8a66b5
Mikkel O. Laursen <m@ikkel.dk>
2019-05-09 23:15:55 +0200

    Add first draft

---
+++ exercise11/src/Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+using Core;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Test message";
+            var bMsg = Encoding.UTF8.GetBytes(msg);
+
+            var chksum = new Checksum(bMsg);
+
+            byte chksumHigh = (byte) ((chksum.Value >> 8) & 0xFF);
+            byte chksumLow = (byte) (chksum.Value & 0xFF);
+            byte seq = 0b1;
+            var type = Core.Type.DATA;
+
+            var header = new Header(chksumHigh, chksumLow, seq, type);
+            var data = new Data(bMsg);
+
+            var packet = new Packet(header, data);
+
+            byte[] bPacket = packet;
+
+            var device = new Serial();
+            var transport = new Transport(device);
+            transport.Send(bPacket);
+        }
+    }
+}
